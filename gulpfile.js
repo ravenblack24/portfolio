e6f4d55a5b93bec80436aaf5e8e30c49617e3ca8
Kay Pea <19981420+ravenblack24@users.noreply.github.com>
2020-09-29 14:12:46 +0100

    added css minify to gulp, alts to images, titles / no referrer to links, added inline fontawesome styles to reduce load time

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
 
 // compile scss into css
 const style = () => {
     // Location of scss files
     return gulp.src('./scss/**/*.scss')
-    // Pass file through sass compiler
+        // Pass file through sass compiler
         .pipe(sass().on('error', sass.logError))
-    // Where to save compiled css file
+        // Generate source maps for Sass to CSS compilation
+        .pipe(sourcemaps.init())
+        //minify file
+        .pipe(cleanCSS())
+        // Generate source maps for Sass to CSS compilation
+        .pipe(sourcemaps.write('./', {includeContent: false, sourceRoot: './scss'}))
+        // Where to save compiled css file
         .pipe(gulp.dest('./css'))
-    // Stream changes to all browsers
+        // Stream changes to all browsers
         .pipe(browserSync.stream())
 }
 
